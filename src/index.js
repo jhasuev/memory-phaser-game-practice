5f9f8be837d51a7656867cf25dbe2ef73f3b4d08
Jamal Hasuev <metafra@mail.ru>
2021-03-17 13:43:11 +0300

    add background

--- src/index.js
+++ src/index.js
@@ -1,2 +1,23 @@
 import './scss/main.scss'
 import "./phaser"
+
+const scene = new Phaser.Scene("Game")
+
+scene.preload = function () {
+    // загрузить изображение
+    scene.load.image("bg", "assets/img/background.png")
+}
+
+scene.create = function () {
+    // вывести изображение
+    scene.add.sprite(0, 0, "bg").setOrigin(0)
+}
+
+const config = {
+    scene,
+    type: Phaser.AUTO,
+    width: 1280,
+    height: 720,
+}
+
+const game = new Phaser.Game(config)
